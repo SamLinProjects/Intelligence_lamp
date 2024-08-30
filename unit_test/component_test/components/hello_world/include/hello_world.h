e9dceb303e401d8de07f3dd0a9a151c8bf0c7711
Sam Lin <samklin33@gmail.com>
2024-08-30 21:18:34 +0800

    component test

---
+++ unit_test/component_test/components/hello_world/include/hello_world.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_WORLD_H
+#define HELLO_WORLD_H
+
+void print_hello_world(void);     // function declaration in .h
+
+#endif // MY_COMPONENT_H
---
+++ unit_test/component_test/components/hello_world/src/hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "hello_world.h" // include header, location specified in CMakeLists.txt
+
+void print_hello_world(void) {    // function definition in .c
+    printf("Hello world!\n");
+}
